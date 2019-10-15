220cbdbbccb5cf00779e420f28efa6eedadc1a81
arkadaw9 <56458648+arkadaw9@users.noreply.github.com>
2019-10-15 13:22:37 -0400

    Update ROA_auto_encoder_temporal_embedding_extract.py

--- Data Generator/ROA_auto_encoder_temporal_embedding_extract.py
+++ Data Generator/ROA_auto_encoder_temporal_embedding_extract.py
@@ -18,29 +18,38 @@ doy=np.zeros(feature_mat.shape[0]);
 
 #convert list to datetime
 all_dates=[dt.datetime.strptime(x,'%Y-%m-%d') for x in all_dates]
+#creating the day of year feature
 for i in range(feature_mat.shape[0]):
     doy[i]=int(all_dates[i].strftime('%j'));
-    
+
+#Concatenating the day of year to feature matrix
 feature_mat = np.column_stack((feature_mat, doy))
 
+#setting the lookback of autoencoder to be 7days
 lookback=7;
 n_features=feature_mat.shape[1];
 
+#reshaping matrix for LSTM encoder input
 mat=np.zeros((feature_mat.shape[0]-lookback,lookback,n_features));
 for i in range(feature_mat.shape[0]-lookback):
     mat[i,:,:]=feature_mat[i:i+lookback,:];
 
 n_obs=mat.shape[0];
+#training on the entire unlabelled data
 train_X = mat[:n_obs, : , :]
-#test_X= mat[n_obs:, :, :]
 
+#normalizing the training set
 mean = train_X.mean(axis=0)
 std = train_X.std(axis=0)
 train_X = (train_X - mean) / std;
 #test_X = (test_X - mean) / std;
 train_Y=train_X[:,-1,:];
 
+#setting the embedding dimension to be 5.
 encoding_dim=5;
+
+
+#Defining the LSTM based auto-encoder
 input1=Input(shape=(lookback, n_features))
 # "encoded" is the encoded representation of the input
 encoded=LSTM(encoding_dim, return_sequences=False)(input1);
@@ -62,16 +71,21 @@ decoder_lstm= autoencoder.layers[-1]
 # create the decoder model
 decoder = Model(encoded_input, decoder_lstm(decoder_layer(encoded_input)))
 
+
+#compiling the autoencoder and training it.
 autoencoder.compile(optimizer='adadelta',loss=mean_squared_error);
 history=autoencoder.fit(train_X,train_Y, epochs=5000, batch_size=256);
 
+#generating the encoder and decoder outputs.
 encoded_out=encoder.predict(train_X);
 decoder_out=decoder.predict(encoded_out);
 #decoder_out=(decoder_out+mean)*std;
 #train_Y=(train_Y+mean)*std;
 err=np.mean(np.square(train_Y-decoder_out));
 embedding_features=encoded_out;
+
+#plotting the training loss
 pyplot.plot(history.history['loss'], label='train_loss')
 pyplot.show();
 
-#spio.savemat('ROA_temporal_feature_autoencoder_lookback_7.mat',{'embedding_features':embedding_features});
+spio.savemat('../Datasets/ROA_temporal_feature_autoencoder_lookback_7.mat',{'embedding_features':embedding_features});
